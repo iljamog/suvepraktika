6afdf1d2a567a55d9ca8885199dc49e434c47a6b
Gleb Stepanov <glebstepan@users.noreply.github.com>
2020-06-09 17:14:14 +0300

    php

---
+++ main.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="import" href="test.html">
+    <script defer src="script.js"></script>
+    <title>Töötukassa</title>
+  </head>
+  <body>
+
+<?php
+	require("header.php");
+  require("vertical_menu.php");
+  require("taotlus_menu.php");
+?>
+
+
