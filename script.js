78b0c1a9d2ec9c05514d90199b508b1d2054da0e
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-06-10 15:23:56 +0300

    Removed unused javascript

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// class UI {
-//   static changeCategory(nextdiv, currentdiv) {
-//     currentdiv.classList.add("hidden");
-//     nextdiv.classList.remove("hidden");
-//     if ((currentdiv = datadiv)) {
-//       nextData = document.querySelector("#nextData");
-//       nextData.addEventListener("click", (e) => {
-//         UI.changeCategory(businessdiv, datadiv);
-//       });
-//     }
-//   }
-// }
-
-// appdiv = document.querySelector("#application");
-// datadiv = document.querySelector("#data");
-// businessdiv = document.querySelector("#application");
-// appdiv = document.querySelector("#application");
-// nextData = document.querySelector("#nextData");
-// document.querySelector("#start").addEventListener("click", (e) => {
-//   UI.changeCategory(datadiv, appdiv);
-// });
-
-// nextData.addEventListener("click", (e) => {
-//   UI.changeCategory(businessdiv, datadiv);
-// });
-
 document.querySelector("#start").addEventListener("click", (e) => {
   window.location.href = "taotlus.html";
 });
