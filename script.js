d9ff7b1c5e883707fa92423221baf35c76263bf8
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-06-11 11:24:15 +0300

    Added functionality to next and previous buttons

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-document.querySelector("#start").addEventListener("click", (e) => {
-  window.location.href = "taotlus.html";
+// document.querySelector("#start").addEventListener("click", (e) => {
+//   window.location.href = "taotlus.html";
+// });
+$("#next_btn_data").click(function () {
+  $("#business-tab").trigger("click");
+});
+
+$("#next_btn_business").click(function () {
+  $("#finance-tab").trigger("click");
+});
+
+$("#previous_btn_business").click(function () {
+  $("#data-tab").trigger("click");
+});
+
+$("#next_btn_finance").click(function () {
+  $("#cv-tab").trigger("click");
+});
+
+$("#previous_btn_finance").click(function () {
+  $("#business-tab").trigger("click");
+});
+
+$("#previous_btn_cv").click(function () {
+  $("#finance-tab").trigger("click");
 });
