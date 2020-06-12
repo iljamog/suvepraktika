5d719747699f23507782f8eec810608d54f10340
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-06-12 14:47:34 +0300

    Moved unused php files

--- taotlus_menu.php
+++
@@ -1,9 +0,0 @@
-<div id="menubar2">
-        <ul>
-          <li><a href="index.html" class="active">Taotlus</a></li>
-          <li><a href="andmed.html">Andmed</a></li>
-          <li><a href="ariplaan.html">Äriplaan</a></li>
-          <li><a href="finants.html">Finantsprognoosid</a></li>
-          <li><a href="cv.html" class="cv">CV</a></li>
-        </ul>
-  </div>
